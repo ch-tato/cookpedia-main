766eeea7dfcd46d36bf374ccdb6e42d623d276a5
chitato <akuquthbi@gmail.com>
2025-11-20 20:17:59 +0700

    Update database connection details for Azure

--- src/main/java/com/cookpedia/util/DBConnection.java
+++ src/main/java/com/cookpedia/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/cookpedia_db?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "!@#$";
+    private static final String URL = "jdbc:mysql://cookpedia-db.mysql.database.azure.com:3306/cookpedia_db?useSSL=true&serverTimezone=UTC";
+    private static final String USER = "cookpedia_admin";
+    private static final String PASSWORD = "HidupJokowi!";
 
     public static Connection getConnection() throws SQLException {
         try {
